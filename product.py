3116a7ac63a575fd70f05605d05cd0e1381b4af3
Jaylen <jjones62202@gmail.com>
2022-04-12 11:12:26 -0400

    Sort function- Fixed price sorting issue

--- product.py
+++ product.py
@@ -6,12 +6,12 @@ class Product:
     def __init__(self, title, description, price):
         self.title = title
         self.description = description
-        self.price = price
+        self.price = int(price)
         self.__class__.productList.append(self)
 
 
     def __repr__(self):
-        return "title:% s description:% s price:% s" % (self.title, self.description, self.price)
+        return "title:% s description:% s price:% d" % (self.title, self.description, self.price)
 
     def get_title(self):
         return self.title
