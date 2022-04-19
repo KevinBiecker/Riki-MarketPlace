b0ecf86e79897020b7b4c54ddba47e7c4e48cda6
SilverBlaze109 <jacobkessinger@live.com>
2022-04-18 20:58:54 -0400

    partially added button to buy partially added auto update when pushing button

--- product.py
+++ product.py
@@ -1,12 +1,14 @@
+from flask import jsonify
 
 
 class Product:
     productList = []
 
-    def __init__(self, title, description, price):
+    def __init__(self, title, description, price, bought):
         self.title = title
         self.description = description
         self.price = int(price)
+        self.bought = int(bought)
         self.__class__.productList.append(self)
 
 
@@ -19,6 +21,14 @@ class Product:
     def get_description(self):
         return self.description
 
+    def buy_item(self):
+        self.bought = 1
 
+    def getJson(self):
 
-
+        data = {
+            'title': self.title,
+            'description': self.description,
+            'price': self.price
+        }
+        return data
